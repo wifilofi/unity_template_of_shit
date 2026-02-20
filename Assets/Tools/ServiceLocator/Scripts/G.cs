3ae0cdb8269af87a0484550a84383b1ee52ce22d
wifilofi <90645177+wifilofi@users.noreply.github.com>
2026-02-20 16:19:26 +0300

    remove stupid namespace

--- Assets/Tools/ServiceLocator/Scripts/G.cs
+++ Assets/Tools/ServiceLocator/Scripts/G.cs
@@ -1,7 +1,6 @@
 ﻿using _Game.Services;
 
-namespace Tools.ServiceLocator.Scripts
-{
+
     public static class G
     {
         public static void InitGlobalServices()
@@ -14,25 +13,24 @@ namespace Tools.ServiceLocator.Scripts
 
         public static T RegGlobal<T>(T service) where T : class
         {
-            global::ServiceLocator.Scripts.ServiceLocator.Global.Register(typeof(T),  service);
+            ServiceLocator.Scripts.ServiceLocator.Global.Register(typeof(T),  service);
             return service;
         }
     
         public static T RegLocal<T>(T service) where T : class
         {
-            global::ServiceLocator.Scripts.ServiceLocator.ForSceneOfCurrent().Register(typeof(T),  service);
+            ServiceLocator.Scripts.ServiceLocator.ForSceneOfCurrent().Register(typeof(T),  service);
             return service;
         }
     
         public static T GetLocal<T>() where T : class
         {
-            return global::ServiceLocator.Scripts.ServiceLocator.ForSceneOfCurrent().Get<T>();
+            return ServiceLocator.Scripts.ServiceLocator.ForSceneOfCurrent().Get<T>();
         }
         public static T GetGlobal<T>() where T : class
         {
-            return global::ServiceLocator.Scripts.ServiceLocator.ForSceneOfCurrent().Get<T>();
+            return ServiceLocator.Scripts.ServiceLocator.ForSceneOfCurrent().Get<T>();
         }
 
         #endregion
     }
-}
